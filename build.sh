12a996958a8eb0f4ed59f6ab082de906708c9c3c
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-02-13 08:46:29 +0700

    Simplified build script

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-make clean
-make package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
-make clean
-make package FINALPACKAGE=1
+make clean package FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
+make clean package FINALPACKAGE=1
 
-make clean
-make package FINALPACKAGE=1 PACKAGE_FORMAT=ipa
-make clean
-make package FINALPACKAGE=1 PACKAGE_FORMAT=ipa UNSANDBOX=1
+make clean package FINALPACKAGE=1 PACKAGE_FORMAT=ipa
+make clean package FINALPACKAGE=1 PACKAGE_FORMAT=ipa UNSANDBOX=1
